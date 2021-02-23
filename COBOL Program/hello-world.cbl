562f1e9c1a34944aa6f4a9f7ff3d65032b86376b
Shyam Dave <52425817+dave2030@users.noreply.github.com>
2021-02-22 23:38:58 -0500

    Create hello-world.cbl

---
+++ COBOL Program/hello-world.cbl
@@ -0,0 +1,19 @@
+      *****************************************************************
+      * Program name:    MYPROG                               
+      * Original author: Shyam                                
+      *
+      * Maintenence Log                                              
+      * Date      Author        Maintenance Requirement               
+      * --------- ------------  --------------------------------------- 
+      * 01/01/08 Shyam  Created for COBOL class         
+      *                                                               
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  MYPROG.
+       AUTHOR. Shyam. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 01/01/08. 
+       DATE-COMPILED. 01/01/08. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
+      display "Hello World"
